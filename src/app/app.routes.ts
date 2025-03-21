90b8eb36a13306e3dd6454390ff2324035e1940e
miit06 <ngthdat10@gmail.com>
2025-03-22 03:10:03 +0700

    Add LoginComponent and edit app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+export const routes: Routes = [
+    {path : '', component: HomePageComponent},
+    {path : 'login', component: LoginPageComponent}
+];
 
-export const routes: Routes = [];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule { }
